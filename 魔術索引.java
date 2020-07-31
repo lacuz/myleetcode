48f15da57214544770a4d405b7a1f66e2c764fe8
lacuz <zhoulianchun@foxmail.com>
2020-07-31 11:32:03 +0800

    魔术索引二分法

--- 魔術索引.java
+++ 魔術索引.java
@@ -1,6 +1,25 @@
 
 class Test{
-    
+    /*魔術索引 二分剪枝叶*/
+    public int findMagicIndex2(int[] nums) {
+        return getAnswer(nums, 0, nums.length - 1);
+    }
+
+    public int getAnswer(int[] nums, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int mid = (right - left) / 2 + left;
+        int leftAnswer = getAnswer(nums, left, mid - 1);
+        if (leftAnswer != -1) {
+            return leftAnswer;
+        } else if (nums[mid] == mid) {
+            return mid;
+        }
+        return getAnswer(nums, mid + 1, right);
+    }
+
+
     /*魔術索引*/
     public static int findMagicIndex(int[] nums) {
         int val = -1;
